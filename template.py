86b87aec3cd65cec1366f8f167353640d8b4f3e5
Hayaka <fisevcik@email.cz>
2022-12-03 17:51:43 +0100

    updated template.py a bit

--- template.py
+++ template.py
@@ -2,21 +2,19 @@ INPUT_FILE = "<number>\input.txt" # change the number because conda runs in root
 #if above not work, use r"C:\Github\Advent-Of-Code\<Number>\input.txt" #conda things
 #input = [list(map(int, (line.strip()))) for line in raw] #Read comp for number
 
-def getInput(name: str) -> list[str]:
-    with open(INPUT_FILE, 'r') as file:
-        raw_input = file.readlines()
-    return raw_input
-    #return [list(map(int, (line.strip()))) for line in raw] #Read comp for number
+def getInput(filename: str) -> list[str]:
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file.read().strip().split("\n")]
 
-def solve1():
+def solve1(filename : str):
     return 1    
 
-def solve2():
+def solve2(filename : str):
     return 2
 
 def main():
-    print(f"solution 1: {solve1()}")
-    print(f"solution 2: {solve2()}")
+    print(f"solution 1: {solve1(INPUT_FILE)}")
+    print(f"solution 2: {solve2(INPUT_FILE)}")
     
 if __name__ == "__main__":
     main()
