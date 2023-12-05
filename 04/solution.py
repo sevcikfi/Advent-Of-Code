d1bd8929aa716b51768317feeefa3192a07b0d84
Hayaka <fisevcik@email.cz>
2023-12-05 02:17:57 +0100

    Day 04 part 2

--- 04/solution.py
+++ 04/solution.py
@@ -6,26 +6,31 @@ TEST_FILE = "04\\test.txt" # test file
 def getInput(filename: str) -> list[str]:
     with open(filename, 'r') as file:
         return [line.strip() for line in file.read().strip().split("\n")]
+
 import re
 def solve1(filename : str):
     score: list[int] = []
     for line in getInput(filename):
         points = 0
         split = re.split(":|\|", line)
-        print(split)
-        c, wins, mine = split
-        for win in tuple(int(wins[i:i+3].strip()) for i in range(0,len(wins.strip()), 3)):
-            if win in tuple(int(mine[i:i+3].strip()) for i in range(0,len(mine.strip()), 3)):
+        for win in tuple(int(split[1][i:i+3].strip()) for i in range(0, len(split[1].strip()), 3)):
+            if win in tuple(int(split[2][i:i+3].strip()) for i in range(0, len(split[2].strip()), 3)):
                 points = points + 1
-        print(points)
-        if points < 1:
-            continue
-        score.append(2**(points-1))
-    print(score)
+        if points > 0:
+            score.append(2**(points-1))
     return sum(score)
 
 def solve2(filename : str):
-    return 2
+    score = [0]*200
+    for i, card in enumerate(getInput(filename)):
+        score[i] = score[i] + 1
+        split = re.split(":|\|", card)
+        hits = 0
+        for win in tuple(int(split[1][i:i+3].strip()) for i in range(0, len(split[1].strip()), 3)):
+            if win in tuple(int(split[2][i:i+3].strip()) for i in range(0, len(split[2].strip()), 3)):
+                hits = hits + 1
+                score[i + hits] = score[i + hits] + score[i]
+    return sum(score)
 
 def Testing(bool=True):
     if not bool: return
@@ -33,7 +38,7 @@ def Testing(bool=True):
 def main():
     assert solve1(TEST_FILE) == 13, "something is wrong xd"
     print(f"solution 1: {solve1(INPUT_FILE)}")
-    assert solve2(TEST_FILE) == 2, "something is wrong 2 xd"
+    assert solve2(TEST_FILE) == 30, "something is wrong 2 xd"
     print(f"solution 2: {solve2(INPUT_FILE)}")
     
 if __name__ == "__main__":
