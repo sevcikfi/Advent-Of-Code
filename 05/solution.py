b91787fbf6b5f2b0eec1ed5f4b26125195b5cf92
Hayaka <fisevcik@email.cz>
2023-12-10 03:38:31 +0100

    Bruteforce naive let it compute

--- 05/solution.py
+++ 05/solution.py
@@ -1,7 +1,7 @@
 DAY = 5  # change the number because conda runs in root of repo
-INPUT_FILE = f"{DAY:02d}\\input.txt" 
+INPUT_FILE = f"{DAY:02d}/input.txt" 
 #if above not work, use r"C:\Github\Advent-Of-Code\<Number>\input.txt" #conda things
-TEST_FILE = f"{DAY:02d}\\test.txt" # test file
+TEST_FILE = f"{DAY:02d}/test.txt" # test file
 
 def getInput(filename: str):
     with open(filename, 'r') as file: #pre split/process input here
@@ -28,31 +28,35 @@ def solve1(filename : str):
         seeds = to_search + new_seeds
     return min(seeds)
 
+import psutil
+from datetime import datetime
 def solve2(filename : str):
     raw_seeds, mappings = getInput(filename)
-    seeds:list[int] = []
+    mins = []
     for i in range(0,len(raw_seeds), 2):
-        new = list(range(raw_seeds[i], raw_seeds[i] + raw_seeds[i+1]))
-        print(new)
-        seeds.extend(new)
+        seeds:list[int] = []
+        seeds = list(range(raw_seeds[i], raw_seeds[i] + raw_seeds[i+1]))
+        print(f"{i//2}: {datetime.now().strftime('%H:%M:%S')}: RAM Used: { psutil.virtual_memory()[3]/1000000000:.4f}GB, {psutil.virtual_memory()[2]}%")
+        for conversion in mappings:
+            new_seeds:list[int] = []
+            to_search:list[int] = []
 
-    print(seeds)
-    for conversion in mappings:
-        new_seeds:list[int] = []
-        to_search:list[int] = []
+            for con in conversion:
+                to_search.clear()
+                dest, sos, lang = [int(c) for c in con.split(" ")]
+                offset = dest - sos
+                for seed in seeds:
+                    if sos <= seed and seed < sos + lang:
+                        new_seeds.append(seed + offset)
+                    else:
+                        to_search.append(seed)
+                seeds = to_search.copy()
+            seeds = to_search + new_seeds
+        print(min(seeds))
+        mins.append(min(seeds))
 
-        for con in conversion:
-            to_search.clear()
-            dest, sos, lang = [int(c) for c in con.split(" ")]
-            offset = dest - sos
-            for seed in seeds:
-                if sos <= seed and seed < sos + lang:
-                    new_seeds.append(seed + offset)
-                else:
-                    to_search.append(seed)
-            seeds = to_search.copy()
-        seeds = to_search + new_seeds
-    return min(seeds)
+    print(mins)
+    return min(mins)
 
 def Testing(bool=True):
     if not bool: return
